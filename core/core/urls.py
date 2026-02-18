e5eac68ebf75d51ffeb0de85a63b02a91f2828ed
JanBash <jan13_2018.29@mail.ru>
2026-02-19 01:08:45 +0600

    superuser

--- core/core/urls.py
+++ core/core/urls.py
@@ -5,8 +5,21 @@ from django.conf import settings
 from django.views.defaults import page_not_found
 from django.conf.urls.static import static
 
+
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def create_admin_view(request):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'ваш_пароль_тут')
+        return HttpResponse("Админ создан!")
+    return HttpResponse("Админ уже существует.")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('init-admin/', create_admin_view),
 
     path('', include('app.urls')),
     path('auth/', include('user.urls')),
@@ -19,16 +32,3 @@ if settings.DEBUG:
     urlpatterns += [
         path('test-404/', page_not_found, {'exception': Exception("Test 404")}),
     ]
-
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
-
-def create_admin_view(request):
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'ваш_пароль_тут')
-        return HttpResponse("Админ создан!")
-    return HttpResponse("Админ уже существует.")
-
-# Добавьте это в список urlpatterns:
-# path('init-admin/', create_admin_view),
